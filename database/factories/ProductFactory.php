54de53bd2559a14dffc6a674086f9f1604b54d82
Arif Hossain <arif.hossain@thedailystar.net>
2020-12-09 21:11:39 +0600

    db seed for product and review

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,10 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->word,
+        'detail' => $faker->paragraph,
+        'price'  => $faker->numberBetween(100,500),
+        'stock'  => $faker->numberBetween(30,100),
+        'discount' => $faker->numberBetween(10,15)
     ];
 });
